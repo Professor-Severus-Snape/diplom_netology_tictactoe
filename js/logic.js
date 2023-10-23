d5b9164fb4a9ad3773541f08066a20afe7e807d3
Julia Fokanova <oncologmma@mail.ru>
2023-10-24 00:15:48 +0300

    fix all diagonal checks with offset

--- js/logic.js
+++ js/logic.js
@@ -1,8 +1,8 @@
 const players = ["x", "o"];
 let activePlayer;
 let board = [];
-const boardSize = 5; // размер квадрата игрового поля
-const winBlock = 3;  // длина выигрышной комбинации
+const boardSize = 5; // здесь можно задать размер квадрата игрового поля!
+const winBlock = 4;  // здесь можно задать длину выигрышной комбинации!
 
 // функция, запускающая новую игру:
 function startGame() {
@@ -11,7 +11,7 @@ function startGame() {
   renderBoard(board);
 }
 
-// функция, создающая квадратное поле любого размера:
+// функция, создающая квадратное поле ${boardSize} размера:
 function createBoard(boardSize) {
   board = [];
   for (let i = 0; i < boardSize; i++) {
@@ -27,48 +27,54 @@ function click(row, col) {
   board[row][col] = players[activePlayer];
   renderBoard(board);
 
-  if (checkWinner(+row, +col)) {
+  if (checkWinner()) {
     showWinner(activePlayer);
   } else {
     changeActivePlayer();
   }
 }
 
-// итоговая проверка выигрыша:
+// проверка выигрыша:
 function checkWinner() { 
-	for (let offsetX = 0; offsetX < 3; offsetX++) { 
-		for (let offsetY = 0; offsetY < 3; offsetY++) {
-			if ( checkDiagonal(offsetX, offsetY) || checkLines(offsetX, offsetY) ) return true;
+  for (let offsetX = 0; offsetX <= (boardSize - winBlock); offsetX++) { 
+		for (let offsetY = 0; offsetY <= (boardSize - winBlock); offsetY++) {
+      if ( checkDiagonalToRight(offsetX, offsetY) || checkDiagonalToLeft(offsetX, offsetY) || checkLines(offsetX, offsetY) ) {
+        return true;
+      }
 		}	
 	}
 	return false; 
 }
 
-// проверка диагоналей - нормально не работает!!!
-function checkDiagonal(offsetX, offsetY) { 
-  let toRight = 1, toLeft = 1;
+// проверка диагонали, идущей слева направо сверху вниз:
+function checkDiagonalToRight(offsetX, offsetY) { 
+  let toRight = 1;
   for (let i = offsetX; i < winBlock + offsetX; i++) {
-		toRight &= (board[i][i] == players[activePlayer]);
-		toLeft &= (board[winBlock + offsetX - i - 1][i] == players[activePlayer]);
-	}
-	// for (let i = 0; i < 3; i++) {
-	// 	toRight &= (board[i][i] == players[activePlayer]);
-	// 	toLeft &= (board[3-i-1][i] == players[activePlayer]);
-	// }
-	return (toRight || toLeft); 
+    toRight &= (board[i][offsetY++] == players[activePlayer]);
+  }
+  return toRight; 
+}
+
+// проверка диагонали, идущей справо налево сверху вниз:
+function checkDiagonalToLeft(offsetX, offsetY) { 
+  let toLeft = 1;
+  for (let i = offsetX; i < winBlock + offsetX; i++) {
+    toLeft &= (board[i][winBlock + offsetX - i - 1 + offsetY] == players[activePlayer]); 
+  }
+  return toLeft; 
 }
 
 // проверка горизонталей и вертикалей:
 function checkLines(offsetX, offsetY) { 
-  let horizont, vertical;
+  let horizontal, vertical;
 	for (let i = offsetX; i < winBlock + offsetX; i++) {
-		horizont = 1;
+		horizontal = 1;
 		vertical = 1;
 		for (let j = offsetY; j < winBlock + offsetY; j++) {
-			horizont &= (board[i][j] == players[activePlayer]);
+			horizontal &= (board[i][j] == players[activePlayer]);
 			vertical &= (board[j][i] == players[activePlayer]);
 		}
-		if (horizont || vertical) {
+		if (horizontal || vertical) {
       return true;
     }
 	}
