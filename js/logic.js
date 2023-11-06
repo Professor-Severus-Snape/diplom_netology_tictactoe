2ca81a48b1c3689160169360bfbdd92b8f219e0b
Julia Fokanova <oncologmma@mail.ru>
2023-11-06 13:02:09 +0300

    change &= to &&=

--- js/logic.js
+++ js/logic.js
@@ -2,7 +2,7 @@ const players = ["x", "o"];
 let activePlayer;
 let board = [];
 const boardSize = 5; // здесь можно задать размер квадрата игрового поля!
-const winBlock = 4;  // здесь можно задать длину выигрышной комбинации!
+const winBlock = 4; // здесь можно задать длину выигрышной комбинации!
 
 // функция, запускающая новую игру:
 function startGame() {
@@ -31,8 +31,8 @@ function click(row, col) {
 
 // проверка выигрыша:
 function checkWinner() {
-  for (let offsetX = 0; offsetX <= (boardSize - winBlock); offsetX++) {
-		for (let offsetY = 0; offsetY <= (boardSize - winBlock); offsetY++) {
+  for (let offsetX = 0; offsetX <= boardSize - winBlock; offsetX++) {
+    for (let offsetY = 0; offsetY <= boardSize - winBlock; offsetY++) {
       if (
         checkDiagonalToRight(offsetX, offsetY) ||
         checkDiagonalToLeft(offsetX, offsetY) ||
@@ -41,16 +41,16 @@ function checkWinner() {
       ) {
         return true;
       }
-		}
-	}
-	return false;
+    }
+  }
+  return false;
 }
 
 // проверка диагонали, идущей слева направо сверху вниз:
 function checkDiagonalToRight(offsetX, offsetY) {
   let toRight = 1;
   for (let i = offsetX; i < winBlock + offsetX; i++) {
-    toRight &= (board[i][offsetY++] == players[activePlayer]);
+    toRight &&= board[i][offsetY++] == players[activePlayer];
   }
   return toRight;
 }
@@ -59,7 +59,7 @@ function checkDiagonalToRight(offsetX, offsetY) {
 function checkDiagonalToLeft(offsetX, offsetY) {
   let toLeft = 1;
   for (let i = offsetX; i < winBlock + offsetX; i++) {
-    toLeft &= (board[i][winBlock + offsetX - i - 1 + offsetY] == players[activePlayer]);
+    toLeft &&= board[i][winBlock + offsetX - i - 1 + offsetY] == players[activePlayer];
   }
   return toLeft;
 }
@@ -67,34 +67,34 @@ function checkDiagonalToLeft(offsetX, offsetY) {
 // проверка горизонтали:
 function checkHorizontal(offsetX, offsetY) {
   let horizontal;
-	for (let i = offsetX; i < winBlock + offsetX; i++) {
-		horizontal = 1;
-		for (let j = offsetY; j < winBlock + offsetY; j++) {
-			horizontal &= (board[i][j] == players[activePlayer]);
-		}
-		if (horizontal) {
+  for (let i = offsetX; i < winBlock + offsetX; i++) {
+    horizontal = 1;
+    for (let j = offsetY; j < winBlock + offsetY; j++) {
+      horizontal &&= board[i][j] == players[activePlayer];
+    }
+    if (horizontal) {
       return true;
     }
-	}
-	return false;
+  }
+  return false;
 }
 
 // проверка вертикали:
-  function checkVertical(offsetX, offsetY) {
-    let vertical;
-  	for (let i = offsetX; i < winBlock + offsetX; i++) {
-  		vertical = 1;
-  		for (let j = offsetY; j < winBlock + offsetY; j++) {
-  			vertical &= (board[j][i] == players[activePlayer]);
-  		}
-  		if (vertical) {
-        return true;
-      }
-  	}
-  	return false;
+function checkVertical(offsetX, offsetY) {
+  let vertical;
+  for (let i = offsetX; i < winBlock + offsetX; i++) {
+    vertical = 1;
+    for (let j = offsetY; j < winBlock + offsetY; j++) {
+      vertical &&= board[j][i] == players[activePlayer];
+    }
+    if (vertical) {
+      return true;
+    }
   }
+  return false;
+}
 
 // функция, передающая ход следующему игроку:
 function changeActivePlayer() {
-  activePlayer ? activePlayer = 0 : activePlayer = 1;
+  activePlayer = activePlayer ? 0 : 1;
 }
